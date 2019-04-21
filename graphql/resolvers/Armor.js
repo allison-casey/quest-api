89de022c263363525a709c1dd18933565cd4f7ca
Steve Casey <stevencasey21@gmail.com>
2019-04-21 18:23:15 -0400

    added armor mutations

--- graphql/resolvers/Armor.js
+++ graphql/resolvers/Armor.js
@@ -6,13 +6,21 @@ export default {
     armors: async (parent, args) => await Armor.find({}).populate()
   },
   Mutation: {
-    addArmor: (root, {id, name, dt, value, weight}) => {
-      const newArmor = new Armor({id, name, dt, value, weight});
-      return new Promise((resolve, reject) => {
-        newArmor.save((err, res) => {
-          err ? reject(err) : resolve(res);
-        });
-      });
-    }
+    addArmor: async (root, {attributes}) => await new Armor(attributes).save(),
+    addArmors: async (root, {attributesList}) =>
+      await Armor.insertMany(attributesList),
+    deleteAllArmors: async (root, args) => await Armor.deleteMany(),
+    deleteArmor: async (root, {id}) => await Armor.findByIdAndRemove(id),
+    updateArmor: async (root, args) =>
+      await Armor.findByIdAndUpdate(args.id, args)
+
+    // addArmor: (root, {id, name, dt, value, weight}) => {
+    //   const newArmor = new Armor({id, name, dt, value, weight});
+    //   return new Promise((resolve, reject) => {
+    //     newArmor.save((err, res) => {
+    //       err ? reject(err) : resolve(res);
+    //     });
+    //   });
+    // }
   }
 };
