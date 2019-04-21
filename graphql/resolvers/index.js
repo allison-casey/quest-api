9866712d494f87875047e120205a6c7ec3d5b36d
Steve Casey <stevencasey21@gmail.com>
2019-04-21 16:41:04 -0400

    moved resolvers up a level

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,8 +1,8 @@
 import {mergeResolvers} from "merge-graphql-schemas";
 
-import Item from "./Item/";
-import Armor from "./Armor/";
-import Weapon from "./Weapon/";
+import Item from "./Item";
+import Armor from "./Armor";
+import Weapon from "./Weapon";
 
 const resolvers = [Armor, Item, Weapon];
 
